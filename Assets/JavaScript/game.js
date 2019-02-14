8c4c17a942001353a0c47ba839e9186f37e9f5c0
candye_sweet <olyamelska@Olhas-MBP.home>
2019-02-13 20:51:44 -0500

    Initial files

---
+++ Assets/JavaScript/game.js
@@ -0,0 +1,86 @@
+let trivia = [
+  {
+    question:
+      "What is the name of the character played by Johnny Depp in the 'Pirates of the Caribbean' film series?",
+    answerOne: "Hector Barbossa",
+    answerTwo: "Davy Jones",
+    answerThree: "Will Turner",
+    correctAnswer: "Jack Sparrow"
+  },
+  {
+    question:
+      "Which actor played Freddie Mercury in the 2018 film 'Bohemian Rhapsody'?",
+    answerOne: "Brian May",
+    answerTwo: "Ben Hardy",
+    answerThree: "Bruno Mars",
+    correctAnswer: "Rami Malek"
+  },
+  {
+    question:
+      "What’s the last thing Jack ever says before he dies in the movie 'A star is born'?",
+    answerOne: "Come here",
+    answerTwo: "Sorry",
+    answerThree: "I will always love you",
+    correctAnswer: "I just want to take another look at you"
+  },
+  {
+    question: "Which of Daenerys Targaryen's three dragons are the largest?",
+    answerOne: "Viserion",
+    answerTwo: "Viserys",
+    answerThree: "Rhaegal",
+    correctAnswer: "Drogon"
+  },
+  {
+    question: "What was wrong with the couch Ross returned to the store?",
+    answerOne: "The colour was wrong",
+    answerTwo: "It had a stain",
+    answerThree: "It was torn",
+    correctAnswer: "It was cut in half"
+  },
+  {
+    question: "'The Stranger Things' is a _____ original.",
+    answerOne: "CW",
+    answerTwo: "HBO",
+    answerThree: "HULU",
+    correctAnswer: "Netflix"
+  },
+  {
+    question:
+      "In which action movie of 2018 major events were happening at Wakanda?",
+    answerOne: "Avengers: Infinity War",
+    answerTwo: "Venom",
+    answerThree: "Aquaman",
+    correctAnswer: "Black Panther"
+  },
+  {
+    question: "What is the name of Hagrid’s big dog?",
+    answerOne: "Crookshanks",
+    answerTwo: "Scabbers",
+    answerThree: "Hedwig",
+    correctAnswer: "Fang"
+  },
+  {
+    question:
+      "Who is the first person to discover the wardrobe at 'Chronicles of Narnia'?",
+    answerOne: "Peter",
+    answerTwo: "Edmund",
+    answerThree: "Susan",
+    correctAnswer: "Lucy"
+  },
+  {
+    question:
+      "What is the name of the pizza company that delivers the pizzas to Kevin and his family?",
+    answerOne: "Domino's",
+    answerTwo: "Little Ceasars",
+    answerThree: "Pizza Hut",
+    correctAnswer: "Little Nero's "
+  }
+];
+
+for (let i = 0; i < trivia.length; i++) {
+  console.log(trivia[i].question);
+  console.log(trivia[i].answerOne);
+  console.log(trivia[i].answerTwo);
+  console.log(trivia[i].answerThree);
+  console.log(trivia[i].correctAnswer);
+}
